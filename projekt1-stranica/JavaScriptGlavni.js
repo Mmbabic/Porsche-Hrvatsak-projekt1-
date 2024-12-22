85b3e08eadf34cdcc304b893dd8770a0a72f5b9e
Sunce Bakino <168968774+Mmbabic@users.noreply.github.com>
2024-12-22 13:48:32 +0100

    Add files via upload

---
+++ projekt1-stranica/JavaScriptGlavni.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll(".carousel-image");
+
+let currentImageIndex = 0;
+
+function changeImage() {
+  images[currentImageIndex].classList.remove("active");
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+
+  images[currentImageIndex].classList.add("active");
+}
+
+setInterval(changeImage, 7000); // 7000 ms = 7 seconds
